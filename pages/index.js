e6d3138abbb7c04c9616339259ee28af8b169795
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-03-12 14:19:03 +0900

    wip API利用

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,42 @@
 import Layout from "../components/layout";
-import MyImage from "../components/image";
+import useSWR from "swr";
 
 export default function Home() {
+  const { data } = useSWR("/data.json");
+
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">Welcome to next.js!</h5>
-          <MyImage fname="img/kadode_logo.png" size="300" />
+          <h5 className="mb-4">
+            {data != undefined ? data.message : "error..."}
+          </h5>
+          <table className="table table-dark">
+            <thead className="">
+              <tr>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data != undefined ? (
+                data.data.map((value, key) => (
+                  <tr key={key}>
+                    <th>{value.name}</th>
+                    <td>{value.mail}</td>
+                    <td>{value.age}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <th></th>
+                  <td>no data.</td>
+                  <td></td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
       </Layout>
     </div>
